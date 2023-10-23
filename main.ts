65f017ac03e5fa7d58c6071aedb38a7db6f8b80f
gbantique <gbantique@gmail.com>
2023-10-23 13:48:16 +0800

    moved main.ts to test.ts for extension purpose

--- main.ts
+++
@@ -1,7 +0,0 @@
-serial.setBaudRate(BaudRate.BaudRate115200)
-Brickcell.iaqInit()
-basic.forever(function () {
-    serial.writeString("tvoc: " + Brickcell.tvoc())
-    serial.writeLine(" | co2eq: " + Brickcell.co2eq())
-    basic.pause(2000)
-})
